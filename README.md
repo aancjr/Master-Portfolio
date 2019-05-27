8321266375a0fa096bda6d5d97c2653ad7234e92
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-26 20:57:32 -0400

    worked on about me page - updated HTML to fix line up issue (misplaced Div). added social links and changed about me profile pic.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Master-Profile
 
 ## Overview:
-    To create a master version of the portfolio website. The idea is design the website based off my person direction and interest, while using various technologies.
+    To create a master version of the portfolio website. The idea is design the website based off my personal direction and interest, while using various technologies.
 
 #Link:
 
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,4 +2,20 @@ $(document).ready(function () {
     $('#my-carousel').carousel({
         interval: false
     });
+
+    // $('.modal-link').on("show.bs.modal", function(e){
+    //     e.preventDefault();
+    //     $('#theModal').modal('show').find('.modal-body').load($(this).attr('href'));
+    //     console.log (this)  
+    // });    
+
+    $("#theModal").on("show.bs.modal", function (e) {
+        var button = $(e.relatedTarget);
+        var modal = $(this);
+    
+        modal.find(".modal-body").load(button.data("remote"));
+        console.log(this)
+    })
 });
+
+
