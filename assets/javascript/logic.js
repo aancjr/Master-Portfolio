98ef98ef42e5bc687066cd4a6baabc370d7c2811
aancjr <45945137+aancjr@users.noreply.github.com>
2019-07-06 22:14:47 -0400

    working on contact form modal. populates, but no content from remote html file. CORS error, also.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,11 +3,17 @@ $(document).ready(function () {
         interval: false
     });
 
+
+    // $('.contactModal').on('click', function(e){
+    //     e.preventDefault();
+    //     $('#theModal').modal('show').find('.modal-content').load($(this).attr('href'));
+    //   });
+
     // $('.modal-link').on("show.bs.modal", function(e){
     //     e.preventDefault();
     //     $('#theModal').modal('show').find('.modal-body').load($(this).attr('href'));
-    //     console.log (this)  
-    // });    
+    //       console.log (this)  
+    // });  
 
     $("#theModal").on("show.bs.modal", function (e) {
         var button = $(e.relatedTarget);
@@ -16,6 +22,20 @@ $(document).ready(function () {
         modal.find(".modal-body").load(button.data("remote"));
         console.log(this)
     })
+
+    // contact form animations
+//   $('#contact').click(function() {
+//     $('#contactForm').fadeToggle();
+//   })
+//   $(document).mouseup(function (e) {
+//     var container = $("#contactForm");
+
+//     if (!container.is(e.target) // if the target of the click isn't the container...
+//         && container.has(e.target).length === 0) // ... nor a descendant of the container
+//     {
+//         container.fadeOut();
+//     }
+//   });
 });
 
 
