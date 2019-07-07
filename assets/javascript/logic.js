ba6e94bf28dbfcfe30471758f27008840a185c5c
aancjr <45945137+aancjr@users.noreply.github.com>
2019-07-07 08:21:59 -0400

    using mailto link in footer for contact. not currently hosted on a server to use remote html pages for contact modal or php send to files - future addition

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,39 +3,6 @@ $(document).ready(function () {
         interval: false
     });
 
-
-    // $('.contactModal').on('click', function(e){
-    //     e.preventDefault();
-    //     $('#theModal').modal('show').find('.modal-content').load($(this).attr('href'));
-    //   });
-
-    // $('.modal-link').on("show.bs.modal", function(e){
-    //     e.preventDefault();
-    //     $('#theModal').modal('show').find('.modal-body').load($(this).attr('href'));
-    //       console.log (this)  
-    // });  
-
-    $("#theModal").on("show.bs.modal", function (e) {
-        var button = $(e.relatedTarget);
-        var modal = $(this);
-    
-        modal.find(".modal-body").load(button.data("remote"));
-        console.log(this)
-    })
-
-    // contact form animations
-//   $('#contact').click(function() {
-//     $('#contactForm').fadeToggle();
-//   })
-//   $(document).mouseup(function (e) {
-//     var container = $("#contactForm");
-
-//     if (!container.is(e.target) // if the target of the click isn't the container...
-//         && container.has(e.target).length === 0) // ... nor a descendant of the container
-//     {
-//         container.fadeOut();
-//     }
-//   });
 });
 
 
