9d9ad32792912656d797de6ed1bd45bf2fb691b3
aancjr <45945137+aancjr@users.noreply.github.com>
2019-07-21 14:43:23 -0400

    renamed master.html to index.html; working on media queries; scrollspy works

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,23 +1,17 @@
-// $(document).ready(function () {
-//     $('#my-carousel').carousel({
-//         interval: false
-//     });
+$(window).scroll(function () {
+  var header = $(document).scrollTop();
+  var headerHeight = $(".header").outerHeight();
+  var firstSection = $(".portfolio div:nth-of-type(1)").outerHeight();
+  if (header > headerHeight) {
+    $(".header").addClass("fixed");
+  } else {
+    $(".header").removeClass("fixed");
+  }
+  if (header > firstSection) {
+    $(".header").addClass("in-view");
+  } else {
+    $(".header").removeClass("in-view");
+  }
+});
 
-// });
-  
-  $(window).scroll(function() {
-    var header = $(document).scrollTop();
-    var headerHeight = $(".header").outerHeight();
-    var firstSection = $(".main-container section:nth-of-type(1)").outerHeight();
-    if (header > headerHeight) {
-      $(".header").addClass("fixed");
-    } else {
-      $(".header").removeClass("fixed");
-    }
-    if (header > firstSection) {
-      $(".header").addClass("in-view");
-    } else {
-      $(".header").removeClass("in-view");
-    }
-  });
-
+$('body').scrollspy({ target: '#navbar-example' })
