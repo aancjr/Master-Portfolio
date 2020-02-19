7a8ecdcef42db60150b78136a133cc3e117b54c0
aancjr <andrewancjr@gmail.com>
2020-02-18 20:17:08 -0500

    revised carousel - using single items/ indicator color set / added JS for consistant card size

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,3 +15,16 @@ $(window).scroll(function () {
 });
 
 $('body').scrollspy({ target: '#navbar-example' })
+
+var cards = $('.card');
+var maxHeight = 0;
+
+for (var i = 0; i < cards.length; i++) {
+  if (maxHeight < $(cards[i]).outerHeight()) {
+    maxHeight = $(cards[i]).outerHeight();
+  }
+};
+
+for (var i = 0; i < cards.length; i++) {
+  $(cards[i]).height(maxHeight);
+};
