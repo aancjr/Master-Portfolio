03f710bbe8404e73ef18ceace3d1eb0a8e1fed37
aancjr <andrewancjr@gmail.com>
2020-02-23 15:22:18 -0500

    updated gifs and card sizes/removed unnecessary html and css

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -23,8 +23,9 @@ for (var i = 0; i < cards.length; i++) {
   if (maxHeight < $(cards[i]).outerHeight()) {
     maxHeight = $(cards[i]).outerHeight();
   }
+  
 };
 
 for (var i = 0; i < cards.length; i++) {
-  $(cards[i]).height(maxHeight);
+  $(cards[i]).outerHeight(maxHeight);
 };
